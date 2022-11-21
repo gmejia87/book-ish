f8367cc77b6fbd5f795afc1a19a6930cf7be6e0d
G Codes <graciela.mejia1@yahoo.com>
2022-11-21 14:27:50 -0800

    update typeDefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,8 +18,26 @@ const typeDefs = gql`
     link: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      authors: [String]
+      description: String
+      title: String!
+      bookId: ID!
+      image: String
+      link: String
+    ): User
   }
 `;
 
