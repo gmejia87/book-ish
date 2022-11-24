0f706c037fbdf451dbafc936873f2e29431d0f70
G Codes <graciela.mejia1@yahoo.com>
2022-11-23 20:00:24 -0800

    update client/src/utils/queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -10,11 +10,10 @@ export const GET_ME = gql`
       savedBooks {
         bookId
         authors
-        image
         description
         title
+        image
         link
-      }
       }
     }
   }
